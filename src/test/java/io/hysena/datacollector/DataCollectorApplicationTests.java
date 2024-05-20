0ca332f799205595ed098f5f936efa1d85ebf805
dbfjru <dbfjru@gmail.com>
2024-05-20 10:57:19 +0900

    init

---
+++ src/test/java/io/hysena/datacollector/DataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hysena.datacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
